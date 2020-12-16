6529cbae90f0fecbbd69e3ad68d76dc06eb9cf9c
lightyears1998 <lightyears1998@hotmail.com>
2020-12-16 22:10:27 +0800

    refactor

--- src/repo/JournalRepository.ts
+++ src/repo/JournalRepository.ts
@@ -9,9 +9,13 @@ import { Journal } from "../entity";
 @Service()
 @EntityRepository(Journal)
 export class JournalRepository extends Repository<Journal> {
-  async findByDay(day: Date): Promise<Journal[]> {
-    const start = moment(day).startOf("day").toDate();
-    const end = moment(day).endOf("day").toDate();
+  async findByDate(date: Date): Promise<Journal[]> {
+    const start = moment(date).startOf("day").toDate();
+    const end = moment(date).endOf("day").toDate();
+    return this.findByDateBetween(start, end);
+  }
+
+  async findByDateBetween(start: Date, end: Date): Promise<Journal[]> {
     return this.find({ date: Between(start, end) });
   }
 }
